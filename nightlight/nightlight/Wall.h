5cd295a8eb03403c9542a0aa5a39791ae938c82f
HerrAndersson <anderssonjonas94@gmail.com>
2015-04-28 12:32:42 +0200

    Moved Door.h to correct folder. Added private scope to all object classes

--- nightlight/nightlight/Wall.h
+++ nightlight/nightlight/Wall.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "GameObject.h"
-class Wall : public GameObject {
+class Wall : public GameObject 
+{
+
+private:
+
+
+
 public:
 	Wall ( XMMATRIX& worldMatrix, RenderObject* renderObject, XMFLOAT3 position, XMFLOAT3 rotation );
 	~Wall ( );
