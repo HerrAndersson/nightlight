8a6f26c7b31fd297f96085a937e4bc75c5c0af02
Rikhard Svensson <rille88@gmail.com>
2015-04-29 01:09:44 +0200

    Added a PushGameObjectToGrid function.

--- nightlight/nightlight/Level.cpp
+++ nightlight/nightlight/Level.cpp
@@ -7,3 +7,14 @@ Level::Level ( ) {
 
 Level::~Level ( ) {
 }
+
+void Level::PushGameObjectToGrid ( int coordX, int coordY, GameObject &content ) {
+	if ( coordX > tileGrid.size ( ) - 1 ) {
+		tileGrid.resize( coordX + 1 );
+	}
+	if ( coordY > tileGrid.at ( coordX ).size ( ) - 1 ) {
+		tileGrid.at ( coordX ).resize ( coordY + 1 );
+	}
+
+	tileGrid[ coordX ][ coordY ].pushGameObjectToTile ( content );
+}
--- nightlight/nightlight/Level.h
+++ nightlight/nightlight/Level.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <vector>
+#include "Tile.h"
+
 class Level 
 {
-
 private:
-
+	std::vector<std::vector<Tile>> tileGrid;
 public:
 	Level ( );
 	~Level ( );
+	void PushGameObjectToGrid ( int coordX, int coordY, GameObject &content );
 };
 
