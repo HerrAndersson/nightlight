32a2fb86d6cde373dd952c2c4d864f2e57916049
jeen13 <thedeaddruid@gmail.com>
2015-05-14 15:32:13 +0200

    Removed all the button functions me and Julia worked hard to create since pointless to have junk code.

--- nightlight/nightlight/Button.h
+++ nightlight/nightlight/Button.h
@@ -12,51 +12,8 @@ public:
 	int getButtonType()  const { return buttonType; }
 	int getButtonWidth() const { return buttonWidth; }
 
-	void ActivateButton();
-	void DeactivateButton();
-
-	void ActivateStartButton();
-	void DeactivateStartButton();
-
-	void setIsStartActivated(bool choice){ isActivated = choice; }
-	bool getIsStartActivated()const { return isActivated; }
-
-	void ActivateContinueButton();
-	void DeactivateContinueButton();
-
-	void setIsContinueActivated(bool choice){ isActivated = choice; }
-	bool getIsContinueActivated()const { return isActivated; }
-
-	void ActivateExitButton();
-	void DeactivateExitButton();
-
-	void setIsExitActivated(bool choice){ isActivated = choice; }
-	bool getIsExitActivated()const { return isActivated; }
-
-	void ClickedStart();
-	void ClickedContinue();
-	bool ClickedEnd();
-
-	void setClickID(int ID) { clickID = ID; }
-	int getClickID()const { return clickID; }
-
-	void setEndGame(bool end) { endgame = end; }
-	bool getEndGame()const { return endgame; }
-
-	//void setMouseClicked(bool choice){ clicked = choice; }
-	//bool getMouseclicked()const { return isActivated;  }
-	//
-	//
-	//int getCoordX() { return coordX; }
-	//int getCoordY() { return coordY; }
-
 private:
 	int buttonType = -1;
 	bool isActivated = false;
-	Button* activatesStartButton = nullptr;
-	Button* activatesContinueButton = nullptr;
-	Button* activatesExitButton = nullptr;
-	Button* mouseclickedd = nullptr;
-	int clickID, buttonWidth;
-	bool endgame;
+	int buttonWidth;
 };
