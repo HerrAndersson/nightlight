e183a1a47f1a674ab93ec38a586b4d864b3f23db
jeen13 <thedeaddruid@gmail.com>
2015-05-15 13:33:20 +0200

    Not much, comments on what is to be done

--- nightlight/nightlight/RenderModule.cpp
+++ nightlight/nightlight/RenderModule.cpp
@@ -838,15 +838,17 @@ bool RenderModule::SetDataPerFrame(XMMATRIX& viewMatrix, XMMATRIX& projectionMat
 	lightPtr->lightAmbientSpot = spotlight->getAmbientColor();
 	lightPtr->lightDiffuseSpot = spotlight->getDiffuseColor();
 	
+	//Point light which only want to exist at Enddoor, therefore needs to get enddoor position (probably without Y to put it at a good height manually):
 	lightPtr->lightDiffusePoint1 = XMFLOAT4(0.95f, 0.1f, 0.2f, 1.0f);
 	lightPtr->lightPosPoint1 = XMFLOAT3(-1.0f, -2.4f, -2.0f);
 
+	//Point light which won't look good but need a bool to only exist in Menu Level:
 	lightPtr->lightDiffusePoint2 = XMFLOAT4(0.55f, 0.45f, 0.2f, 1.0f);
 	lightPtr->lightPosPoint2 = XMFLOAT3(-7.0f, -2.7f, -5.5f) ;
 
+	//For good looking lights this information below, but it is for a spotlight and that is annoying to deal with:
 	//Menu Spotlight: Position: -7.0f, -0.7f, -0.5f For Complete lit up room: (-7.0f, -8.0f, 5.0f)
 	//Menu Spotlight: Direction: 0.0008f, 0.0f, -0.99f For Complete lit up room: (0.0008f, 0.5f, -0.99f)
-
 	//Best Position: XMFLOAT3(-7.0f, -2.3f, -1.0f);
 	//Best Direction: XMFLOAT3(0.0008f, 0.3f, -0.99f);
 
